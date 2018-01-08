964fa79064669d412f53e10ffd6ba84847c8a548
choustof <petit.christophe.91@gmail.com>
2018-01-08 10:37:48 +0100

    Changements dtabse model

--- Database/database_model.js
+++ Database/database_model.js
@@ -2,11 +2,10 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-
-host:'127.0.0.1',
-user:'root',
-password:'',
-database:'unfalldb'
+	host:'us-cdbr-iron-east-05.cleardb.net',
+	user:'bfff06ee812fa3',
+	password:'9e17ad3a',
+	database:'heroku_ab5fb20c85f8647'
 
 
 });
