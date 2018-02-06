851d3220dba41150913230459416a7805a1d39b3
keva91 <keva.91@hotmail.fr>
2018-02-06 09:14:55 +0100

    little change

--- Database/database_model.js
+++ Database/database_model.js
@@ -2,19 +2,13 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-<<<<<<< HEAD
 
 host:'us-cdbr-iron-east-05.cleardb.net',
 user:'bfff06ee812fa3',
 password:'9e17ad3a',
 database:'heroku_ab5fb20c85f8647'
-=======
-	host:'us-cdbr-iron-east-05.cleardb.net',
-	user:'bfff06ee812fa3',
-	password:'9e17ad3a',
-	database:'heroku_ab5fb20c85f8647'
->>>>>>> 964fa79064669d412f53e10ffd6ba84847c8a548
 
+	
 
 });
 
